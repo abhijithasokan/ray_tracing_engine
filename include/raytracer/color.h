c3e7643e4c950090aa242e017ccc27be853abd9f
Abhijith Asokan <abhijithasokan27@gmail.com>
2025-12-01 10:03:29 +0100

    Add gamma correction

--- include/raytracer/color.h
+++ include/raytracer/color.h
@@ -6,3 +6,4 @@
 
 using Color = Vec3;
 void write_color(std::ostream& out, const Color& cc);
+double linear_to_gamma(double linear_intensity);
--- src/color.cc
+++ src/color.cc
@@ -1,10 +1,19 @@
 #include <raytracer/color.h>
 #include <raytracer/common.h>
 
+
+double linear_to_gamma(double linear_intensity) {
+    // using gamma = 2.0
+    if(linear_intensity > 0.0)
+        return std::sqrt(linear_intensity);
+    
+    return 0.0;
+}
+
 void write_color(std::ostream& out, const Color& cc) {
-    auto r = cc.x();
-    auto g = cc.y();
-    auto b = cc.z();
+    auto r = linear_to_gamma(cc.x());
+    auto g = linear_to_gamma(cc.y());
+    auto b = linear_to_gamma(cc.z());
 
     static const Interval intensity(0.000, 0.999);
     int rbyte = int(256 * intensity.clamp(r));
